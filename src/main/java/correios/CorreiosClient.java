71bd7036fc8f956829e2a309f028fc87d28408ea
William Brombal Chinelato <williamb@ciandt.com>
2017-01-29 15:07:09 -0200

    Removed all code that are not generated or suggested in the Spring WS article.

--- src/main/java/correios/CorreiosClient.java
+++ src/main/java/correios/CorreiosClient.java
@@ -7,31 +7,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
 
-import correios.domain.model.CepSearchResponse;
 import correios.wsdl.ConsultaCEP;
 import correios.wsdl.ConsultaCEPResponse;
+import correios.wsdl.EnderecoERP;
 
 public class CorreiosClient extends WebServiceGatewaySupport {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CorreiosClient.class);
 
-	public Object getAddress(final String cep) {
+	@SuppressWarnings("unchecked")
+	public EnderecoERP getAddress(final String cep) {
 		final ConsultaCEP request = new ConsultaCEP();
 		request.setCep(cep);
 
-		LOGGER.info("Requesting quote for " + cep);
+		LOGGER.info("Searching for zip code: " + cep);
 
-		final Object response = getWebServiceTemplate().marshalSendAndReceive(request);
+		final JAXBElement<ConsultaCEPResponse> response = (JAXBElement<ConsultaCEPResponse>) getWebServiceTemplate().marshalSendAndReceive(request);
+		LOGGER.info("JAXB: {} - {}", response.getValue().getClass().getName(), ToStringBuilder.reflectionToString(response.getValue()));
 
-		if (response instanceof JAXBElement) {
-			final JAXBElement<?> element = (JAXBElement<?>) response;
-			LOGGER.info("JAXB: {} - {}", element.getValue().getClass().getName(), ToStringBuilder.reflectionToString(element.getValue()));
-
-			return ((ConsultaCEPResponse) element.getValue()).getReturn();
-		}
-
-		LOGGER.info("Object: {} - {}", response.getClass().getName(), ToStringBuilder.reflectionToString(response));
-		return ((CepSearchResponse) response).getAddress();
+		return response.getValue().getReturn();
 	}
 
 }
--- src/main/java/correios/CorreiosConfiguration.java
+++ src/main/java/correios/CorreiosConfiguration.java
@@ -4,17 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import correios.domain.model.CepSearchResponse;
-import correios.wsdl.ConsultaCEP;
-
 @Configuration
 public class CorreiosConfiguration {
 
 	@Bean
 	public Jaxb2Marshaller marshaller() {
 		final Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		// marshaller.setContextPath("correios.wsdl");
-		marshaller.setClassesToBeBound(ConsultaCEP.class, CepSearchResponse.class);
+		marshaller.setContextPath("correios.wsdl");
 
 		return marshaller;
 	}
